795dbd861e7d6a12d5f36536914359e944d26961
Unknown <leahnallen@outlook.com>
2018-01-07 17:35:40 -0800

    About Functionality

---
+++ FrontEnd (Security Mvc)/NW_Central_Library/LibraryModels/About.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NW_Central_Library.LibraryModels
+{
+    public partial class About
+    {
+        public int Id { get; set; }
+        public string LibraryName { get; set; }
+        public string LibraryDescr { get; set; }
+
+    }
+}
